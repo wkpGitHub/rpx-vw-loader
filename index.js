85666f0707d6342e49862cd08fadbd44a60f3a3d
wkpGitHub <1441962654@qq.com>
2021-12-22 10:28:19 +0800

    优化性能

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = function(source) {
   // 获取到用户给当前 Loader 传入的 options
   const options = loaderUtils.getOptions(this) || {};
   const designWidth = options.designWidth || 750;
+  const scale = 100 / designWidth;
   
   var reg = /\b(\d+(\.\d+)?)rpx\b/g;
   return source.replace(reg, (match, $1) => {
-    const scale = 100 / designWidth;
     return `${($1* scale).toFixed(6)}vw`;
   });
 };
