2b35690c716932457a18f560091023c756a9f46a
wkpGitHub <1441962654@qq.com>
2021-12-22 10:25:54 +0800

    提交核心代码

---
+++ index.js
@@ -0,0 +1,13 @@
+const loaderUtils = require('loader-utils');
+
+module.exports = function(source) {
+  // 获取到用户给当前 Loader 传入的 options
+  const options = loaderUtils.getOptions(this) || {};
+  const designWidth = options.designWidth || 750;
+  
+  var reg = /\b(\d+(\.\d+)?)rpx\b/g;
+  return source.replace(reg, (match, $1) => {
+    const scale = 100 / designWidth;
+    return `${($1* scale).toFixed(6)}vw`;
+  });
+};
